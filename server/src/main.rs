fb2828dcf9239a54f5541dcf183ba51522863ebb
Carlos Juarez <carlosgj94@gmail.com>
2024-09-14 10:56:38 +0300

    feat: Displaying the port when running the server

--- server/src/main.rs
+++ server/src/main.rs
@@ -21,6 +21,7 @@ async fn main() {
             .allow_methods([Method::GET, Method::POST, Method::OPTIONS]),
     );
 
+    println!("Listening on: http://0.0.0.0:3001");
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
